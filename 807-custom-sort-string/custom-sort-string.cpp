253570fadff28e6243f06ae90a9345d6aded1405
railav61 <146355352+railav61@users.noreply.github.com>
2024-03-12 04:06:53 +0530

    Time: 4 ms (26.84%) | Memory: 7.7 MB (53.64%) - LeetSync

--- 807-custom-sort-string/custom-sort-string.cpp
+++ 807-custom-sort-string/custom-sort-string.cpp
@@ -3,7 +3,7 @@ public:
     string customSortString(string order, string s) {
        int a = order.size();
         int b = s.size();
-        // char x;
+        char x;
         string s1="";
         int count=0;
         for(int i=0;i<a;i++){
@@ -14,8 +14,8 @@ public:
                 }
             }
             for(int k=0;k<count;k++){
-                // x = order[i];
-                s1.push_back(order[i]);
+                x = order[i];
+                s1.push_back(x);
             }
             count=0;
         }
