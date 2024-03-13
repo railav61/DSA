b183dc29d1b92cf06523591ae874df0d87bebdca
railav61 <146355352+railav61@users.noreply.github.com>
2024-03-13 10:36:22 +0530

    Time: 37 ms (10.80%) | Memory: 10.3 MB (5.18%) - LeetSync

---
+++ 2571-find-the-pivot-integer/find-the-pivot-integer.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int pivotInteger(int n) {
+        vector<int> a;
+        int x=0,y=0;
+        if(n==1){
+            return 1;
+        }
+        for(int i=0;i<n;i++){
+            a.push_back(i+1);
+        }
+        for(int i=0;i<n;i++){
+            x+=a[i];
+            for(int j=i;j<n;j++){
+                y+=a[j];
+            }
+            if(x==y){
+                return a[i];
+            }
+            else{
+                y=0;
+            }
+        }
+        return -1;
+    }
+};
